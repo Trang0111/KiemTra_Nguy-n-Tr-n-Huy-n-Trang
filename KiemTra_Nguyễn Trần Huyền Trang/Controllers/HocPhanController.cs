d6e44eb7df63a1f35fb15655ed00f2d1f37ad0a1
nguyentranhuyentrang0111 <nguyentranhuyentrang0111@gmail.com>
2022-06-13 16:12:35 +0700

    Add project files.

---
+++ KiemTra_Nguyễn Trần Huyền Trang/Controllers/HocPhanController.cs
@@ -0,0 +1,21 @@
+﻿using KiemTra_Nguyễn_Trần_Huyền_Trang.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KiemTra_Nguyễn_Trần_Huyền_Trang.Controllers
+{
+    public class HocPhanController : Controller
+    {
+        // GET: HocPhan
+
+        MyDataDataContext data = new MyDataDataContext();
+        public ActionResult DanhSachHocPhan()
+        {
+            var all_hocphan = from ss in data.HocPhans select ss;
+            return View(all_hocphan);
+        }
+    }
+}
---
+++ KiemTra_Nguyễn Trần Huyền Trang/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KiemTra_Nguyễn_Trần_Huyền_Trang.MvcApplication" Language="C#" %>
